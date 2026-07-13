cf9cfbb1b248b22d0e228c8d5d00ae78f69fed4a
itsfortestlol-sudo <its.for.testlol@gmail.com>
2026-07-13 14:08:10 +0600

    Update main.ino

--- main.ino
+++ main.ino
@@ -24,7 +24,7 @@ const long  GMT_OFFSET_SEC  = 21600;
 const int   DST_OFFSET_SEC  = 0;
 const char* NTP_SERVER      = "asia.pool.ntp.org";
 
-const int   CURRENT_VERSION = 2;
+const int   CURRENT_VERSION = 3;
 const char* VERSION_URL     = "https://raw.githubusercontent.com/itsfortestlol-sudo/Esp32/main/virsion.txt";
 const char* BIN_URL         = "https://raw.githubusercontent.com/itsfortestlol-sudo/Esp32/main/firmware.bin";
 
