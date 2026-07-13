d24e4e3dc9e35f88837350c22716885914958d0a
itsfortestlol-sudo <its.for.testlol@gmail.com>
2026-07-13 14:24:43 +0600

    Create secret.h

---
+++ secret.h
@@ -0,0 +1,12 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+// WiFi Credentials
+const char* WIFI_SSID = "Afia Mahir";
+const char* WIFI_PASS = "afiamahir2026";
+
+// Gemini API Configuration
+const char* GEMINI_API_KEY = "AQ.Ab8RN6KqxIMLahKONQULFEc00-0y1I8zCmqk7iXhwaJPKQYHLg";
+const char* GEMINI_HOST = "generativelanguage.googleapis.com";
+
+#endif // SECRETS_H
